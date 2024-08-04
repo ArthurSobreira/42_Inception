b7b65a39f6f6dd7db6d82c1fa3648493e0b6a3cf
Arthur <tutusobreira@gmail.com>
2024-08-04 00:06:55 -0300

    Update db_init.sh

--- src/requirements/mariadb/tools/db_init.sh
+++ src/requirements/mariadb/tools/db_init.sh
@@ -3,6 +3,11 @@
 #------------------ mariadb start ------------------#
 service mariadb start
 
+# Just because I'm don't make the docker-compose file yet
+WP_DATABASE=wordpress_database
+WP_USER=arsobrei
+WP_PASSWORD=anicepassword@
+
 #------------------ mariadb config -----------------#
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${WP_DATABASE};"
 mariadb -u root -e "CREATE USER '${WP_USER}'@'%' IDENTIFIED BY '${WP_PASSWORD}';"
